ac414297395043211c06cf1f0dea9fd0edacff10
Josh Goebel <me@joshgoebel.com>
2021-12-14 21:41:16 -0500

    largest series product

---
+++ largest-series-product/largest_series_product.rb
@@ -0,0 +1,22 @@
+class Series
+  def initialize(digits)
+    @digits = digits
+  end
+
+  def largest_product(size)
+    raise ArgumentError if size.negative?
+    raise ArgumentError unless valid_digits?
+    raise ArgumentError if size > @digits.length
+    return 1 if @digits.length.zero? || size.zero?
+
+    @digits.each_char.map(&:to_i).each_cons(size).map do |list|
+      list.inject(1) { |acc, n| acc * n }
+    end.max
+  end
+
+  private
+
+  def valid_digits?
+    @digits.match(/^\d*$/)
+  end
+end
