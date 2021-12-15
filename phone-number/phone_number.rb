4fc1673bc08e6e7fd6855b0e30c02be607b887ad
Josh Goebel <me@joshgoebel.com>
2021-12-14 22:06:56 -0500

    wip

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -1,15 +1,15 @@
 class PhoneNumber
+  SPACE_or_PUNC = /[\(\). -]*/
   PHONE_RE = /
     ^
-    [^\d]
-    *1?
-    [^\d]*
-    (\d{3})
-    [^\d]*
-    (\d{3})
-    [^\d]*
-    (\d{4})
-    [^\d]*
+    \+?1?       # international prefix
+    #{SPACE_or_PUNC}
+    (\d{3})   # area code
+    #{SPACE_or_PUNC}
+    (\d{3})   # exchange code
+    #{SPACE_or_PUNC}
+    (\d{4})   # last 4 digits
+    \s*
     $
   /x
   private_constant :PHONE_RE
@@ -19,14 +19,14 @@ class PhoneNumber
   end
 
   def clean
-    m, area_code, prefix, local = PHONE_RE.match(@number).to_a
+    m, area_code, exchange, local = PHONE_RE.match(@number).to_a
     return nil if m.nil?
     # area code may not start with 0 or 1
     return nil if %w[0 1].include?(area_code.chr)
-    # prefix may not start with 0 or 1
-    return nil if %w[0 1].include?(prefix.chr)
+    # exchange may not start with 0 or 1
+    return nil if %w[0 1].include?(exchange.chr)
 
-    [area_code, prefix, local].join("")
+    [area_code, exchange, local].join("")
   end
 
   private
